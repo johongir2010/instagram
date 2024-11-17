0f611cce07c775b88f3e377e4d8b596116619df1
johongir2010 <jabdulazizov24@gmail.com>
2024-11-17 11:32:58 +0500

    first commit

--- user/views.py
+++ user/views.py
@@ -1,5 +1,11 @@
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from datetime import datetime
+from .models import NEW, CODE_VERIFIED
 
 from .models import User
 from .serializers import SignUpSerializer
@@ -12,3 +18,45 @@ class CreateUserView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (permissions.AllowAny, )
     serializer_class = SignUpSerializer
+
+
+
+class VerifyApi(APIView):
+    parser_classes = (IsAuthenticated,)
+
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        code = request.get('code')
+
+
+        self.check_verify(user, code)
+        return Response(
+            data = {
+                'success': True,
+                'auth_statud': user.auth_status,
+                'access': user.token()['refresh_token']
+            }
+        )
+
+
+    @staticmethod
+    def check_verify(user, code):
+        verify = user.verify_code.filter(expiration_time__gte=datetime.now(), code=code, is_confirmed=False).exists()
+        if not verify:
+            data = {
+                'message': 'Code xato yoki amal qilish muddati otib ketgan'
+            }
+            raise ValidationError(data)
+        else:
+            user.updated(is_confirmed=True)
+
+
+        if user.auth_status == NEW:
+            user.auth_status = CODE_VERIFIED
+            user.save()
+
+
+
+        return user
+
